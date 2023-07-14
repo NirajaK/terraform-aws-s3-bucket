00966e83255fa5b9b00880c10ffc16f33c1b058b
NirajaK <niraja.kulkarni@gmail.com>
2023-07-14 12:36:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
     bucket_key_enabled = var.bucket_key_enabled
 
     apply_server_side_encryption_by_default {
-      sse_algorithm     = var.sse_algorithm
+      sse_algorithm     = "aws:kms"
       kms_master_key_id = var.kms_master_key_arn
     }
   }
