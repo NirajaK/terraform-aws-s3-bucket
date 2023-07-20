99a2678540c01eefe01f5547bef006df91534554
Tenable.cs <admin@accurics.com>
2023-07-20 10:08:18 +0530

    Auto Remediation by Tenable.cs, file: main.tf

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
     bucket_key_enabled = var.bucket_key_enabled
 
     apply_server_side_encryption_by_default {
-      kms_master_key_id = "test"
+      kms_master_key_id = "fixed"
     }
   }
 }
